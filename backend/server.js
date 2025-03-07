d4f613a6ed3e1969a359fa820290c353622420bd
Hritam <hritam.dutta@gmail.com>
2025-03-07 07:33:17 -0800

    7

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors()); // Allow all preflight OPTIONS requests
 app.use(express.json()); // Middleware to parse JSON
 
 // MongoDB Connection
