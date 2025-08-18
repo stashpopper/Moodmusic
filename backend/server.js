80458b72208083f3666b015975e7221339de40b2
Hritam Brahmachari <hritambrahmachari1@gmail.com>
2025-08-17 18:10:30 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ if (isProduction) {
   
   // Additional CORS headers for production
   app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://themoodmusic.netlify.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://moodandmusic.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
